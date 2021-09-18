bc3d460ebeb5c6d35649d74e7856f0995e1f87cd
ZhenLuoLF <90990036+ZhenLuoLF@users.noreply.github.com>
2021-09-18 18:47:42 -0400

    For Assignment1
    Finished the code for assignment_1

--- src/main/java/edu/neu/coe/info6205/randomwalk/RandomWalk.java
+++ src/main/java/edu/neu/coe/info6205/randomwalk/RandomWalk.java
@@ -3,7 +3,6 @@
  */
 
 package edu.neu.coe.info6205.randomwalk;
-
 import java.util.Random;
 
 public class RandomWalk {
@@ -21,6 +20,8 @@ public class RandomWalk {
      */
     private void move(int dx, int dy) {
         // TO BE IMPLEMENTED
+        x = x + dx;
+        y = y + dy;
     }
 
     /**
@@ -30,6 +31,11 @@ public class RandomWalk {
      */
     private void randomWalk(int m) {
         // TO BE IMPLEMENTED
+        while(m > 0)
+        {
+            randomMove();
+            m = m - 1;
+        }
     }
 
     /**
@@ -49,7 +55,8 @@ public class RandomWalk {
      */
     public double distance() {
         // TO BE IMPLEMENTED
-        return 0;
+        double ds = Math.sqrt(Math.pow(x,2) + Math.pow(y,2));
+        return ds;
     }
 
     /**
@@ -70,6 +77,7 @@ public class RandomWalk {
     }
 
     public static void main(String[] args) {
+/*
         if (args.length == 0)
             throw new RuntimeException("Syntax: RandomWalk steps [experiments]");
         int m = Integer.parseInt(args[0]);
@@ -77,6 +85,16 @@ public class RandomWalk {
         if (args.length > 1) n = Integer.parseInt(args[1]);
         double meanDistance = randomWalkMulti(m, n);
         System.out.println(m + " steps: " + meanDistance + " over " + n + " experiments");
-    }
+*/
+        int n = 100;
+        int m = 1;
+        for(int i = 1; i < 11; i++) {
+            m = 10 * m;
+            for (int j = 0; j < 10; j++) {
+                double meanDistance = randomWalkMulti(m, n);
+                System.out.println(m + " steps: " + meanDistance + " over " + n + " experiments");
+            }
+        }
 
+    }
 }
